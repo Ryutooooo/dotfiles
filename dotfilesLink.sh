8f70a38a24dca96c6a9e87512f2be3ab942c93aa
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2020-01-03 23:40:37 +0900

    feat: create starship toml

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,9 @@ do
     [ "$f" = ".DS_Store" ] && continue
     [ "$f" = ".gitignore" ] && continue
     [ "$f" = ".gitmodules" ] && continue
+    [ "$f" = "starship.toml" ] && continue
 
     ln -snfv "dotfiles"/"$f" "$HOME"/"$f"
 done
+
+ln -snfv "$HOME"/"dotfiles"/starship.toml "$HOME"/.config/starship.toml
