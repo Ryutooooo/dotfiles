8ec039c61fbe0fed5f67f411cfe6243e23bd7f05
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2021-03-21 22:14:11 +0900

    rem: unnecessary spaces

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,13 +2,13 @@
 
 for f in .??*
 do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".DS_Store" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
-    [ "$f" = "starship.toml" ] && continue
+  [ "$f" = ".git" ] && continue
+  [ "$f" = ".DS_Store" ] && continue
+  [ "$f" = ".gitignore" ] && continue
+  [ "$f" = ".gitmodules" ] && continue
+  [ "$f" = "starship.toml" ] && continue
 
-    ln -snfv `pwd`/"$f" "$HOME"/"$f"
+  ln -snfv `pwd`/"$f" "$HOME"/"$f"
 done
 
 ln -snfv `pwd`/starship.toml "$HOME"/.config/starship.toml
