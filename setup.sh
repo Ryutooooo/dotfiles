19bb12d59bd33885f405754a7142d3b49df840d0
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2021-09-25 20:35:48 +0900

    rem vim plug

--- setup.sh
+++ setup.sh
@@ -7,7 +7,3 @@ echo "created symbolic link"
 echo "brew installed"
 
 brew bundle install --file=.brewfile
-echo "installed your libraries and apps"
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
