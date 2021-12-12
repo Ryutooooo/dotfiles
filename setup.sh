3772222f6a9b568fa345c3787971e536d2f0abd0
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2021-12-12 21:41:22 +0900

    fix: update initial command

--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ section() {
 
 section "get Cica"
 curl -OL https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
-section"installed Cica"
-unzip Cica_v5.0.2_with_emoji.zip
 section "unzip font files"
+unzip Cica_v5.0.2_with_emoji.zip
 
 sh ./install-fonts.sh
 
 section "get git-completion.bash"
 curl -OL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
-git clean -f 
 section "clean up unnecessary files"
+git clean -f 
 
-sh ./dotfilesLink.sh
 section "created symbolic link"
+sh ./dotfilesLink.sh
 
-# $(brew --prefix)/opt/fzf/install
+section "configure fzf to bash"
+yes | $(brew --prefix)/opt/fzf/install
