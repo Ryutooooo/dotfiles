28173b89b9c67e7104e906a273094c90884071bb
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2022-02-11 18:08:55 +0900

    feat: changed neovim color scheme

--- init.vim
+++ init.vim
@@ -28,7 +28,7 @@ Plug 'iamcco/markdown-preview.nvim', { 'do': 'cd app && yarn install'  }
 Plug 'neovim/nvim-lspconfig'
 
 " color scheme
-Plug 'projekt0n/github-nvim-theme'
+Plug 'bluz71/vim-moonfly-colors'
 
 " enhancing syntax hightlight
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
--- vim/colorscheme.lua
+++ vim/colorscheme.lua
@@ -1,16 +1,19 @@
-require("github-theme").setup({
-  theme_style = "dark_default",
-  function_style = "italic",
-  sidebars = {"qf", "vista_kind", "terminal", "packer"},
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = {hint = "orange", error = "#ff0000"},
-  -- Overwrite the highlight groups
-  overrides = function(c)
-    return {
-      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-      -- this will remove the highlight groups
-      TSField = {},
-    }
-  end
-})
+
+vim.cmd [[colorscheme moonfly]]
+
+-- require("github-theme").setup({
+--   theme_style = "dark_default",
+--   function_style = "italic",
+--   sidebars = {"qf", "vista_kind", "terminal", "packer"},
+--   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+--   colors = {hint = "orange", error = "#ff0000"},
+--   -- Overwrite the highlight groups
+--   overrides = function(c)
+--     return {
+--       htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+--       DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+--       -- this will remove the highlight groups
+--       TSField = {},
+--     }
+--   end
+-- })
